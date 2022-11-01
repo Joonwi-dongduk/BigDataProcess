59a8a1469ee229afc636449b5c3ec3a33ae97f5b
Joonwi-dongduk <115362027+Joonwi-dongduk@users.noreply.github.com>
2022-11-01 20:50:25 +0900

    Update UBERStudent20190996.py

--- HW3/UBERStudent20190996.py
+++ HW3/UBERStudent20190996.py
@@ -5,20 +5,37 @@ input = sys.argv[1]
 output = sys.argv[2]
 
 file = open(output, "wt")
-weekday = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+week = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+weekday = [{}, {}, {}, {}, {}, {}, {}]
+
+
 with open(input, "rt") as f:
 	while True:
 		line = f.readline()
 		if not line:
 			break
+		
 		cont = line.split(',')
 		num = cont[0]
 		date = cont[1].split('/')
-		vehicles = cont[2]
-		trips = cont[3]
 
 		import calendar
 		day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
-		file.write("%s,%s %s,%s" %(num, weekday[day], vehicles, trips))
+		if num in weekday[day].keys():
+			vehicles = int(weekday[day][num].split(',')[0]) + int(cont[2])
+			trips = int(weekday[day][num].split(',')[1]) + int(cont[3])
+			weekday[day][num] = str(vehicles) + ',' + str(trips)
+		else:
+			weekday[day][num] = cont[2] + ',' + cont[3]
+		weekday[day][num]
+
+		vehicles = cont[2]
+		trips = cont[3]
+
+#		file.write("%s,%s %s,%s" %(num, weekday[day], vehicles, trips))
+
+for n in range(7):
+	for w in weekday[n].keys():
+		file.write("%s,%s %s\n" %(w, week[n], weekday[n][w]))
 
 file.close()
