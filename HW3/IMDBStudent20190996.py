c1824d26bb2e223c365ed2a191e4ac331ee45dff
Joonwi-dongduk <115362027+Joonwi-dongduk@users.noreply.github.com>
2022-11-01 14:01:17 +0900

    Update IMDBStudent20190996.py

--- HW3/IMDBStudent20190996.py
+++ HW3/IMDBStudent20190996.py
@@ -1,14 +1,25 @@
+#usr/bin/python3
+
 import sys
 input = sys.argv[1]
 output = sys.argv[2]
 
 file = open(output, "wt")
+genList = {}
 with open(input, "rt") as f:
 	while True:
-		line = f.readline().split('::')
-		if not line[0]:
+		line = f.readline()
+		if not line:
 			break
-		title = line[1][0:-7]
-		id = line[0]
-		file.write("%s %s\n" %(title, id))
+		cont = line.split('::')
+		genre = cont[2].split('|')
+		for g in genre:
+			if '\n' in g:
+				g = g[:-1]
+			if g in genList.keys():
+				genList[g] += 1
+			else:
+				genList[g] = 1
+for g in genList.keys():
+	file.write("%s %d\n" %(g, genList[g]))
 file.close()
