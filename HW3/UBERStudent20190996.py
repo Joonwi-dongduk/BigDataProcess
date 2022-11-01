694d3b59a1e788e583405e8da22e9ee34533997f
Joonwi-dongduk <115362027+Joonwi-dongduk@users.noreply.github.com>
2022-11-01 14:02:08 +0900

    Update UBERStudent20190996.py

--- HW3/UBERStudent20190996.py
+++ HW3/UBERStudent20190996.py
@@ -8,18 +8,17 @@ file = open(output, "wt")
 weekday = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 with open(input, "rt") as f:
 	while True:
-		line = f.readline().split(',')
-		if not line[0]:
+		line = f.readline()
+		if not line:
 			break
-
-		num = line[0]
-		date = line[1].split('/')
-		vehicles = line[2]
-		trips = line[3]
+		cont = line.split(',')
+		num = cont[0]
+		date = cont[1].split('/')
+		vehicles = cont[2]
+		trips = cont[3]
 
 		import calendar
 		day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
 		file.write("%s,%s %s,%s" %(num, weekday[day], vehicles, trips))
-	#	print("%s,%s %s,%s" %(num, weekday[day], vehicles, trips), end='')
 
 file.close()
